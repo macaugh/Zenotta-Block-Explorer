0e43f4e58916ce5fc2d49c7963ebeca3ddd3c1ac
Byron Houwens <zhion360@gmail.com>
2021-04-06 12:26:24 +0200

    Complete block explorer template

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -8,44 +8,68 @@ function Table() {
         <table className={styles.blockTable}>
             <thead>
                 <tr>
+                    <th>Timeout At</th>
                     <th>Block Number</th>
                     <th>Participating Compute Nodes</th>
                     <th>Participating Mining Nodes</th>
-                    <th>Timeout At</th>
                     <th>Transactions</th>
                     <th>Combined Reward</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
+                    <td>2021/03/04 21:44:51</td>
                     <td>546</td>
                     <td>8</td>
                     <td>40613</td>
-                    <td>2021/03/04 21:44:51</td>
                     <td>1092</td>
                     <td>223</td>
                 </tr>
                 <tr>
+                    <td>2021/03/04 21:44:51</td>
                     <td>546</td>
                     <td>8</td>
                     <td>40613</td>
-                    <td>2021/03/04 21:44:51</td>
                     <td>1092</td>
                     <td>223</td>
                 </tr>
                 <tr>
+                    <td>2021/03/04 21:44:51</td>
                     <td>546</td>
                     <td>8</td>
                     <td>40613</td>
-                    <td>2021/03/04 21:44:51</td>
                     <td>1092</td>
                     <td>223</td>
                 </tr>
                 <tr>
+                    <td>2021/03/04 21:44:51</td>
                     <td>546</td>
                     <td>8</td>
                     <td>40613</td>
+                    <td>1092</td>
+                    <td>223</td>
+                </tr>
+                <tr>
                     <td>2021/03/04 21:44:51</td>
+                    <td>546</td>
+                    <td>8</td>
+                    <td>40613</td>
+                    <td>1092</td>
+                    <td>223</td>
+                </tr>
+                <tr>
+                    <td>2021/03/04 21:44:51</td>
+                    <td>546</td>
+                    <td>8</td>
+                    <td>40613</td>
+                    <td>1092</td>
+                    <td>223</td>
+                </tr>
+                <tr>
+                    <td>2021/03/04 21:44:51</td>
+                    <td>546</td>
+                    <td>8</td>
+                    <td>40613</td>
                     <td>1092</td>
                     <td>223</td>
                 </tr>
