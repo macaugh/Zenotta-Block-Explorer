7418b2a5daa1eea082025131c4e641b9d72fa454
BHouwens <zhion360@gmail.com>
2022-09-16 11:34:38 +0200

    Fix for newer Receipt transactions

--- src/components/TxView/TxView.tsx
+++ src/components/TxView/TxView.tsx
@@ -1,191 +1,276 @@
-import * as React from 'react';
-import { useObserver } from 'mobx-react';
-import { useParams } from 'react-router-dom';
-import { StoreContext } from '../../index';
+import * as React from "react";
+import { useObserver } from "mobx-react";
+import { useParams } from "react-router-dom";
+import { StoreContext } from "../../index";
 
-import styles from './TxView.scss';
-import { TxInfo } from '../TxInfo/TxInfo';
-import { downloadFile, formatCsvTxs, itemToCsv } from '../../formatCsv';
-import { formatNumber } from '../../formatData';
-import { Card } from 'components/Card/Card';
-import { Block, Input, InputInfo, Output, ReceiptInfo, StackData, TokenInfo, Transaction, TransactionInfo } from 'interfaces';
+import styles from "./TxView.scss";
+import { TxInfo } from "../TxInfo/TxInfo";
+import { downloadFile, formatCsvTxs, itemToCsv } from "../../formatCsv";
+import { formatNumber } from "../../formatData";
+import { Card } from "components/Card/Card";
+import {
+  Block,
+  Input,
+  InputInfo,
+  Output,
+  ReceiptInfo,
+  StackData,
+  TokenInfo,
+  Transaction,
+  TransactionInfo,
+  OutputValueV2,
+} from "interfaces";
 
 export const TxView = () => {
-    let { hash } = useParams<any>();
-    const store = React.useContext(StoreContext);
-    const [localData, setLocalData] = React.useState<TransactionInfo | null>(null);
-    const [mainTxData, setMainTxData] = React.useState<any>(null);
+  let { hash } = useParams<any>();
+  const store = React.useContext(StoreContext);
+  const [localData, setLocalData] = React.useState<TransactionInfo | null>(
+    null
+  );
+  const [mainTxData, setMainTxData] = React.useState<any>(null);
 
-    const checkSeenTxIns = (t: Input, seenIns: string[]) => {
-        if (t.previousOut && t.previousOut.tHash) {
-            let tHash = t.previousOut.tHash;
+  const checkSeenTxIns = (t: Input, seenIns: string[]) => {
+    if (t.previousOut && t.previousOut.tHash) {
+      let tHash = t.previousOut.tHash;
 
-            if (seenIns.indexOf(tHash) == -1) {
-                seenIns.push(tHash);
-                return true;
-            }
-            return false;
+      if (seenIns.indexOf(tHash) == -1) {
+        seenIns.push(tHash);
+        return true;
+      }
+      return false;
+    }
+    return false;
+  };
+
+  const formatScript = (stack: StackData[]) => {
+    let combo = [];
+    for (let entry of stack) {
+      let key: any = Object.keys(entry)[0];
+      let val = (entry as any)[key];
+
+      combo.push(val instanceof Array ? toHexString(val) : val);
+    }
+    return combo.join("\n");
+  };
+
+  const formatTransactionInputs = (inputs: Input[]) => {
+    return inputs.map((input) => {
+      const previousOutHash =
+        input.previousOut && input.previousOut.tHash
+          ? input.previousOut.tHash
+          : "N/A";
+      return {
+        previousOutHash: previousOutHash,
+        scriptSig: formatScript(input.scriptSig.stack),
+      };
+    });
+  };
+
+  const formatTransactionOutputs = (
+    outputs: Output[]
+  ): TokenInfo[] | ReceiptInfo[] => {
+    return outputs.map((output: Output) => {
+      if (output.value.hasOwnProperty("Token")) {
+        // is a token output
+        const tokens = (output.value as { Token: number }).Token;
+        return {
+          address: output.scriptPubKey,
+          tokens: `${formatNumber((tokens / 25200).toFixed(2))} ZENO`,
+          fractionatedTokens: `${formatNumber(tokens)}`,
+          lockTime: output.locktime,
+        } as TokenInfo;
+      } else if (output.value.hasOwnProperty("Receipt")) {
+        // is a Receipt output
+        const receipt = output.value;
+
+        if (typeof (receipt as { Receipt: number }).Receipt === "number") {
+          const receipts = (output.value as { Receipt: number }).Receipt;
+          return {
+            address: output.scriptPubKey,
+            receipts: receipts,
+            lockTime: output.locktime,
+            genesisTransactionHash: "N/A",
+          } as ReceiptInfo;
         }
-        return false;
-    };
 
-    const formatScript = (stack: StackData[]) => {
-        let combo = [];
-        for (let entry of stack) {
-            let key: any = Object.keys(entry)[0];
-            let val = (entry as any)[key];
+        if ("amount" in (receipt as { Receipt: OutputValueV2 }).Receipt) {
+          const amount = (receipt as { Receipt: OutputValueV2 }).Receipt.amount;
+          const drsTxHash = (receipt as { Receipt: OutputValueV2 }).Receipt
+            .drs_tx_hash;
 
-            combo.push(val instanceof Array ? toHexString(val) : val);
-        }
-        return combo.join('\n');
-    };
-
-    const formatTransactionInputs = (inputs: Input[]) => {
-        return inputs.map((input) => {
-            const previousOutHash = input.previousOut && input.previousOut.tHash ? input.previousOut.tHash : 'N/A';
-            return {
-                previousOutHash: previousOutHash,
-                scriptSig: formatScript(input.scriptSig.stack),
-            };
-        });
-    };
-
-    const formatTransactionOutputs = (outputs: Output[]): TokenInfo[] | ReceiptInfo[] => {
-        return outputs.map((output: Output) => {
-            if (output.value.hasOwnProperty('Token')) { // is a token output
-                const tokens = (output.value as { Token: number }).Token;
-                return {
-                    address: output.scriptPubKey,
-                    tokens: `${formatNumber((tokens / 25200).toFixed(2))} ZENO`,
-                    fractionatedTokens: `${formatNumber(tokens)}`,
-                    lockTime: output.locktime,
-                } as TokenInfo;
-            } else if (output.value.hasOwnProperty('Receipt')) { // is a Receipt output
-                const receipts = (output.value as { Receipt: number }).Receipt;
-                return {
-                    address: output.scriptPubKey,
-                    receipts: receipts,
-                    lockTime: output.locktime,
-                } as ReceiptInfo;
-            }
-        }) as TokenInfo[] | ReceiptInfo[];
-    };
-
-    const formatTransactions = (transactions: Transaction[], hashes: string[]) => {
-        if (!transactions || !transactions.length) {
-            return [];
+            console.log('drsTxHash', drsTxHash);
+          return {
+            address: output.scriptPubKey,
+            receipts: amount,
+            lockTime: output.locktime,
+            genesisTransactionHash: drsTxHash || "N/A",
+          } as ReceiptInfo;
         }
 
-        if (transactions.length == 1) {
-            let tx = transactions[0];
-            return extractTransactionInfo(tx, hashes, 0);
-        }
+        return {
+          address: output.scriptPubKey,
+          receipts: 0,
+          lockTime: output.locktime,
+          genesisTransactionHash: "N/A",
+        };
+      }
+    }) as TokenInfo[] | ReceiptInfo[];
+  };
 
-        return transactions.map((tx, i) => {
-            return extractTransactionInfo(tx, hashes, i);
-        });
-    };
-
-    const formatDataForTable = (localData: any) => {
-        if (!localData) { return null }
-        return Object.keys(localData).map(key => {
-            const value = localData[key];
-            if (key === 'previousOutHash' && value != 'N/A') { // Add clickable link if displayed as a block transaction
-                return {
-                    heading: key,
-                    value: <a href={`/tx/${value}`}>{value}</a>
-                };
-            } else {
-                return {
-                    heading: key,
-                    value: value
-                };
-            }
-        });
+  const formatTransactions = (
+    transactions: Transaction[],
+    hashes: string[]
+  ) => {
+    if (!transactions || !transactions.length) {
+      return [];
     }
 
-    const formatIncomingData = (tx: Transaction | null): TransactionInfo | null => {
-        if (tx) {
-            setMainTxData(formatTransactions([tx], [hash]));
+    if (transactions.length == 1) {
+      let tx = transactions[0];
+      return extractTransactionInfo(tx, hashes, 0);
+    }
 
-            return {
-                inputs: formatTransactionInputs(tx.inputs as Input[]),
-                outputs: formatTransactionOutputs(tx.outputs as Output[]),
-            };
-        }
-        return null
-    };
+    return transactions.map((tx, i) => {
+      return extractTransactionInfo(tx, hashes, i);
+    });
+  };
 
-    const extractTransactionInfo = (tx: Transaction, hashes: string[], index: number) => {
-        let seenIns: string[] = [];
+  const formatDataForTable = (localData: any) => {
+    if (!localData) {
+      return null;
+    }
+    return Object.keys(localData).map((key) => {
+      const value = localData[key];
+      if (key === "previousOutHash" && value != "N/A") {
+        // Add clickable link if displayed as a block transaction
         return {
-            hash: hashes[index],
-            totalTokens: formatNumber(tx.outputs.reduce((acc: number, o: Output) => acc + (o.value as { Token: number }).Token, 0)),
-            txInHashes: tx.inputs.filter((t: Input) => checkSeenTxIns(t, seenIns)).map((i: Input) => i.previousOut && i.previousOut.tHash),
-            outputs: tx.outputs.map((o: Output) => {
-                return {
-                    publicKey: o.scriptPubKey,
-                    lockTime: o.locktime,
-                    tokens: formatNumber((o.value as { Token: number }).Token),
-                };
-            }),
+          heading: key,
+          value: <a href={`/tx/${value}`}>{value}</a>,
         };
+      } else {
+        return {
+          heading: key,
+          value: value,
+        };
+      }
+    });
+  };
+
+  const formatIncomingData = (
+    tx: Transaction | null
+  ): TransactionInfo | null => {
+    if (tx) {
+      setMainTxData(formatTransactions([tx], [hash]));
+
+      return {
+        inputs: formatTransactionInputs(tx.inputs as Input[]),
+        outputs: formatTransactionOutputs(tx.outputs as Output[]),
+      };
+    }
+    return null;
+  };
+
+  const extractTransactionInfo = (
+    tx: Transaction,
+    hashes: string[],
+    index: number
+  ) => {
+    let seenIns: string[] = [];
+    return {
+      hash: hashes[index],
+      totalTokens: formatNumber(
+        tx.outputs.reduce(
+          (acc: number, o: Output) =>
+            acc + (o.value as { Token: number }).Token,
+          0
+        )
+      ),
+      txInHashes: tx.inputs
+        .filter((t: Input) => checkSeenTxIns(t, seenIns))
+        .map((i: Input) => i.previousOut && i.previousOut.tHash),
+      outputs: tx.outputs.map((o: Output) => {
+        return {
+          publicKey: o.scriptPubKey,
+          lockTime: o.locktime,
+          tokens: formatNumber((o.value as { Token: number }).Token),
+        };
+      }),
     };
+  };
 
-    const toHexString = (byteArray: number[]) => {
-        return Array.from(byteArray, (byte) => {
-            return ('0' + (byte & 0xff).toString(16)).slice(-2);
-        }).join('');
-    };
+  const toHexString = (byteArray: number[]) => {
+    return Array.from(byteArray, (byte) => {
+      return ("0" + (byte & 0xff).toString(16)).slice(-2);
+    }).join("");
+  };
 
-    // const downloadTx = async () => {
-    //     if (localData) {
-    //         const { txs, headers } = formatCsvTxs([localData]);
-    //         const csv = itemToCsv(txs[0]);
-    //         downloadFile(`tx-${hash}`, csv);
-    //     }
-    // };
+  // const downloadTx = async () => {
+  //     if (localData) {
+  //         const { txs, headers } = formatCsvTxs([localData]);
+  //         const csv = itemToCsv(txs[0]);
+  //         downloadFile(`tx-${hash}`, csv);
+  //     }
+  // };
 
-    React.useEffect(() => {
-        if (!localData) {
-            store.fetchBlockchainItem(hash).then((fetchedData: Transaction | Block | null) => {
-                setLocalData(formatIncomingData(fetchedData ? fetchedData as Transaction : null));
-            });
-        }
-    }, []);
+  React.useEffect(() => {
+    if (!localData) {
+      store
+        .fetchBlockchainItem(hash)
+        .then((fetchedData: Transaction | Block | null) => {
+          setLocalData(
+            formatIncomingData(
+              fetchedData ? (fetchedData as Transaction) : null
+            )
+          );
+        });
+    }
+  }, []);
 
-    React.useEffect(() => {
-        if (localData && window.location.hash) {
-            let elmnt = document.getElementById(window.location.hash.substring(1));
-            if (elmnt) elmnt.scrollIntoView(true);
-        }
-    }, [localData]);
+  React.useEffect(() => {
+    if (localData && window.location.hash) {
+      let elmnt = document.getElementById(window.location.hash.substring(1));
+      if (elmnt) elmnt.scrollIntoView(true);
+    }
+  }, [localData]);
 
-    return useObserver(() => (
-        <div className={styles.container}>
-            <h2 className={styles.heading}>
-                {'Transaction'}
-            </h2>
-            {/* <CsvBtn action={() => downloadTx()} /> */}
-            <div className={styles.txContainer}>
-                {mainTxData !== null && mainTxData !== undefined && <TxInfo {...mainTxData} txView={true} />}
+  return useObserver(() => (
+    <div className={styles.container}>
+      <h2 className={styles.heading}>{"Transaction"}</h2>
+      {/* <CsvBtn action={() => downloadTx()} /> */}
+      <div className={styles.txContainer}>
+        {mainTxData !== null && mainTxData !== undefined && (
+          <TxInfo {...mainTxData} txView={true} />
+        )}
 
-                {localData && localData.inputs && localData.inputs.length > 0 &&
-                    <>
-                        <h2 id="inputs">Inputs</h2>
-                        {localData.inputs.map((input: InputInfo, i: number) => {
-                            return <div className={styles.infoContainer} key={i} ><Card rows={formatDataForTable(input)} /></div>;
-                        })}
-                    </>}
+        {localData && localData.inputs && localData.inputs.length > 0 && (
+          <>
+            <h2 id="inputs">Inputs</h2>
+            {localData.inputs.map((input: InputInfo, i: number) => {
+              return (
+                <div className={styles.infoContainer} key={i}>
+                  <Card rows={formatDataForTable(input)} />
+                </div>
+              );
+            })}
+          </>
+        )}
 
-                {localData && localData.outputs && localData.outputs.length > 0 &&
-                    <div>
-                        <h2 id='outputs'>Outputs</h2>
-                        {localData && (localData.outputs as any).map((output: TokenInfo | ReceiptInfo, i: number) => {
-                            return <div className={styles.infoContainer} key={i} ><Card rows={formatDataForTable(output)} /></div>;
-                        })}
-                    </div>}
-            </div>
-        </div>
-    ));
+        {localData && localData.outputs && localData.outputs.length > 0 && (
+          <div>
+            <h2 id="outputs">Outputs</h2>
+            {localData &&
+              (localData.outputs as any).map(
+                (output: TokenInfo | ReceiptInfo, i: number) => {
+                  return (
+                    <div className={styles.infoContainer} key={i}>
+                      <Card rows={formatDataForTable(output)} />
+                    </div>
+                  );
+                }
+              )}
+          </div>
+        )}
+      </div>
+    </div>
+  ));
 };
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,165 +1,173 @@
 /** Global */
 export enum ItemType {
-    Block,
-    Transaction
+  Block,
+  Transaction,
 }
 
 /** Request data structures */
 
 export interface BlockDataWrapperV0_1 {
-    block: BlockDataV0_1,
-    mining_tx_hash_and_nonces: any
+  block: BlockDataV0_1;
+  mining_tx_hash_and_nonces: any;
 }
 
 export interface BlockDataV0_1 {
-    header: {
-        version: number,
-        bits: number,
-        nonce: number[],
-        b_num: number,
-        seed_value: number[],
-        previous_hash: string | null,
-        merkle_root_hash: string,
-    },
-    transactions: string[]
+  header: {
+    version: number;
+    bits: number;
+    nonce: number[];
+    b_num: number;
+    seed_value: number[];
+    previous_hash: string | null;
+    merkle_root_hash: string;
+  };
+  transactions: string[];
 }
 
 export interface BlockDataV2 {
-    header: {
-        version: number,
-        bits: number,
-        nonce_and_mining_tx_hash: any[],
-        b_num: number,
-        seed_value: number[],
-        previous_hash: string,
-        txs_merkle_root_and_hash: string[],
-    },
-    transactions: string[]
+  header: {
+    version: number;
+    bits: number;
+    nonce_and_mining_tx_hash: any[];
+    b_num: number;
+    seed_value: number[];
+    previous_hash: string;
+    txs_merkle_root_and_hash: string[];
+  };
+  transactions: string[];
 }
 
 export interface TransactionData {
-    druid_info: null,
-    inputs: InputData[],
-    outputs: OutputData[],
-    version: number
+  druid_info: null;
+  inputs: InputData[];
+  outputs: OutputData[];
+  version: number;
 }
 
 export interface InputData {
-    previous_out: {
-        n: number,
-        t_hash: string
-    },
-    script_signature: {
-        stack: StackData[]
-    }
+  previous_out: {
+    n: number;
+    t_hash: string;
+  };
+  script_signature: {
+    stack: StackData[];
+  };
 }
 
 export interface OutputData {
-    drs_block_hash: string | null,
-    drs_tx_hash: string | null,
-    locktime: number,
-    script_public_key: string,
-    value: { Token: number } | { Receipt: number }
+  drs_block_hash: string | null;
+  drs_tx_hash: string | null;
+  locktime: number;
+  script_public_key: string;
+  value: { Token: number } | { Receipt: number };
 }
 
 export interface StackData {
-    Op?: string,
-    Num?: number,
-    Bytes?: string,
-    Signature?: number[],
-    PubKey?: number[]
+  Op?: string;
+  Num?: number;
+  Bytes?: string;
+  Signature?: number[];
+  PubKey?: number[];
 }
 
 /** Application interfaces */
 
 export interface Block {
-    bNum: number,
-    previousHash: string,
-    seed: number[],
-    bits: number,
-    version: number,
-    miningTxHashNonces: {
-        hash: string,
-        nonce: number[],
-    },
-    merkleRootHash: {
-        merkleRootHash: string,
-        txsHash: string
-    },
-    transactions: string[],
+  bNum: number;
+  previousHash: string;
+  seed: number[];
+  bits: number;
+  version: number;
+  miningTxHashNonces: {
+    hash: string;
+    nonce: number[];
+  };
+  merkleRootHash: {
+    merkleRootHash: string;
+    txsHash: string;
+  };
+  transactions: string[];
 }
 
 export interface Transaction {
-    druidInfo: null,
-    inputs: Input[],
-    outputs: Output[],
-    version: number
+  druidInfo: null;
+  inputs: Input[];
+  outputs: Output[];
+  version: number;
 }
 
 export interface Input {
-    previousOut: {
-        num: number,
-        tHash: string
-    } | null,
-    scriptSig: {
-        stack: StackData[]
-    }
+  previousOut: {
+    num: number;
+    tHash: string;
+  } | null;
+  scriptSig: {
+    stack: StackData[];
+  };
 }
 
 export interface Output {
-    drsBHash: string | null,
-    drsTHash: string | null,
-    locktime: number,
-    scriptPubKey: string,
-    value: { Token: number } | { Receipt: number }
+  drsBHash: string | null;
+  drsTHash: string | null;
+  locktime: number;
+  scriptPubKey: string;
+  value:
+    | { Token: number }
+    | { Receipt: number }
+    | { Token: OutputValueV2 }
+    | { Receipt: OutputValueV2 };
+}
+
+export interface OutputValueV2 {
+  amount: number;
+  drs_tx_hash: string | null;
 }
 
 /** UI Interfaces */
 export interface BlockInfo {
-    hash: string,
-    bNum: number,
-    merkleRootHash: string,
-    previousHash: string,
-    version: number,
-    byteSize: string,
-    nbTransactions: number
+  hash: string;
+  bNum: number;
+  merkleRootHash: string;
+  previousHash: string;
+  version: number;
+  byteSize: string;
+  nbTransactions: number;
 }
 
 export interface TransactionInfo {
-    inputs: {
-        previousOutHash: string,
-        scriptSig: string,
-    }[],
-    outputs: TokenInfo[] | ReceiptInfo[],
+  inputs: {
+    previousOutHash: string;
+    scriptSig: string;
+  }[];
+  outputs: TokenInfo[] | ReceiptInfo[];
 }
 
 export interface InputInfo {
-    previousOutHash: string,
-    scriptSig: string,
+  previousOutHash: string;
+  scriptSig: string;
 }
 
 export interface TokenInfo {
-    address: string,
-    tokens: string,
-    fractionatedTokens: string,
-    lockTime: number,
+  address: string;
+  tokens: string;
+  fractionatedTokens: string;
+  lockTime: number;
 }
 
 export interface ReceiptInfo {
-    address: string,
-    receipts: number,
-    lockTime: number,
+  address: string;
+  receipts: number;
+  lockTime: number;
+  genesisTransactionHash: string;
 }
 
-export interface TransactionTableData{
-    blockNum: number,
-    hash: string,
-    transaction: Transaction
+export interface TransactionTableData {
+  blockNum: number;
+  hash: string;
+  transaction: Transaction;
 }
 
-export interface BlockTableData{
-    block: Block,
-    hash: string
+export interface BlockTableData {
+  block: Block;
+  hash: string;
 }
-
-
